00144f80bf4509b96495413883fb96705e0c7980
nvillemin <nvillemin@users.noreply.github.com>
2014-07-19 18:04:38 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Hitman2Statistics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LB_ShotsFired = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.LB_Running = new System.Windows.Forms.Label();
+            this.NB_ShotsFired = new System.Windows.Forms.Label();
+            this.NB_CloseEncounters = new System.Windows.Forms.Label();
+            this.LB_CloseEncounters = new System.Windows.Forms.Label();
+            this.NB_Alerts = new System.Windows.Forms.Label();
+            this.LB_Alerts = new System.Windows.Forms.Label();
+            this.NB_EnemiesKilled = new System.Windows.Forms.Label();
+            this.LB_EnemiesKilled = new System.Windows.Forms.Label();
+            this.NB_InnocentsKilled = new System.Windows.Forms.Label();
+            this.LB_InnocentsKilled = new System.Windows.Forms.Label();
+            this.LB_Time = new System.Windows.Forms.Label();
+            this.LB_MapName = new System.Windows.Forms.Label();
+            this.NB_Headshots = new System.Windows.Forms.Label();
+            this.LB_Headshots = new System.Windows.Forms.Label();
+            this.NB_EnemiesHarmed = new System.Windows.Forms.Label();
+            this.LB_EnemiesHarmed = new System.Windows.Forms.Label();
+            this.NB_InnocentsHarmed = new System.Windows.Forms.Label();
+            this.LB_InnocentsHarmed = new System.Windows.Forms.Label();
+            this.LB_Timer = new System.Windows.Forms.Label();
+            this.LB_Mission = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LB_ShotsFired
+            // 
+            this.LB_ShotsFired.AutoSize = true;
+            this.LB_ShotsFired.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_ShotsFired.Location = new System.Drawing.Point(46, 99);
+            this.LB_ShotsFired.Name = "LB_ShotsFired";
+            this.LB_ShotsFired.Size = new System.Drawing.Size(86, 20);
+            this.LB_ShotsFired.TabIndex = 0;
+            this.LB_ShotsFired.Text = "Shots fired";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // LB_Running
+            // 
+            this.LB_Running.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Running.ForeColor = System.Drawing.Color.Red;
+            this.LB_Running.Location = new System.Drawing.Point(12, 12);
+            this.LB_Running.Name = "LB_Running";
+            this.LB_Running.Size = new System.Drawing.Size(278, 23);
+            this.LB_Running.TabIndex = 1;
+            this.LB_Running.Text = "HITMAN 2 IS NOT RUNNING";
+            this.LB_Running.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NB_ShotsFired
+            // 
+            this.NB_ShotsFired.AutoSize = true;
+            this.NB_ShotsFired.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_ShotsFired.ForeColor = System.Drawing.Color.Blue;
+            this.NB_ShotsFired.Location = new System.Drawing.Point(12, 99);
+            this.NB_ShotsFired.Name = "NB_ShotsFired";
+            this.NB_ShotsFired.Size = new System.Drawing.Size(19, 20);
+            this.NB_ShotsFired.TabIndex = 7;
+            this.NB_ShotsFired.Tag = "Value";
+            this.NB_ShotsFired.Text = "0";
+            // 
+            // NB_CloseEncounters
+            // 
+            this.NB_CloseEncounters.AutoSize = true;
+            this.NB_CloseEncounters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_CloseEncounters.ForeColor = System.Drawing.Color.Blue;
+            this.NB_CloseEncounters.Location = new System.Drawing.Point(12, 119);
+            this.NB_CloseEncounters.Name = "NB_CloseEncounters";
+            this.NB_CloseEncounters.Size = new System.Drawing.Size(19, 20);
+            this.NB_CloseEncounters.TabIndex = 9;
+            this.NB_CloseEncounters.Tag = "Value";
+            this.NB_CloseEncounters.Text = "0";
+            // 
+            // LB_CloseEncounters
+            // 
+            this.LB_CloseEncounters.AutoSize = true;
+            this.LB_CloseEncounters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_CloseEncounters.Location = new System.Drawing.Point(46, 119);
+            this.LB_CloseEncounters.Name = "LB_CloseEncounters";
+            this.LB_CloseEncounters.Size = new System.Drawing.Size(133, 20);
+            this.LB_CloseEncounters.TabIndex = 8;
+            this.LB_CloseEncounters.Text = "Close encounters";
+            // 
+            // NB_Alerts
+            // 
+            this.NB_Alerts.AutoSize = true;
+            this.NB_Alerts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_Alerts.ForeColor = System.Drawing.Color.Blue;
+            this.NB_Alerts.Location = new System.Drawing.Point(12, 159);
+            this.NB_Alerts.Name = "NB_Alerts";
+            this.NB_Alerts.Size = new System.Drawing.Size(19, 20);
+            this.NB_Alerts.TabIndex = 13;
+            this.NB_Alerts.Tag = "Value";
+            this.NB_Alerts.Text = "0";
+            // 
+            // LB_Alerts
+            // 
+            this.LB_Alerts.AutoSize = true;
+            this.LB_Alerts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Alerts.Location = new System.Drawing.Point(46, 159);
+            this.LB_Alerts.Name = "LB_Alerts";
+            this.LB_Alerts.Size = new System.Drawing.Size(50, 20);
+            this.LB_Alerts.TabIndex = 12;
+            this.LB_Alerts.Text = "Alerts";
+            // 
+            // NB_EnemiesKilled
+            // 
+            this.NB_EnemiesKilled.AutoSize = true;
+            this.NB_EnemiesKilled.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_EnemiesKilled.ForeColor = System.Drawing.Color.Blue;
+            this.NB_EnemiesKilled.Location = new System.Drawing.Point(12, 179);
+            this.NB_EnemiesKilled.Name = "NB_EnemiesKilled";
+            this.NB_EnemiesKilled.Size = new System.Drawing.Size(19, 20);
+            this.NB_EnemiesKilled.TabIndex = 15;
+            this.NB_EnemiesKilled.Tag = "Value";
+            this.NB_EnemiesKilled.Text = "0";
+            // 
+            // LB_EnemiesKilled
+            // 
+            this.LB_EnemiesKilled.AutoSize = true;
+            this.LB_EnemiesKilled.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_EnemiesKilled.Location = new System.Drawing.Point(46, 179);
+            this.LB_EnemiesKilled.Name = "LB_EnemiesKilled";
+            this.LB_EnemiesKilled.Size = new System.Drawing.Size(112, 20);
+            this.LB_EnemiesKilled.TabIndex = 14;
+            this.LB_EnemiesKilled.Text = "Enemies Killed";
+            // 
+            // NB_InnocentsKilled
+            // 
+            this.NB_InnocentsKilled.AutoSize = true;
+            this.NB_InnocentsKilled.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_InnocentsKilled.ForeColor = System.Drawing.Color.Blue;
+            this.NB_InnocentsKilled.Location = new System.Drawing.Point(12, 219);
+            this.NB_InnocentsKilled.Name = "NB_InnocentsKilled";
+            this.NB_InnocentsKilled.Size = new System.Drawing.Size(19, 20);
+            this.NB_InnocentsKilled.TabIndex = 17;
+            this.NB_InnocentsKilled.Tag = "Value";
+            this.NB_InnocentsKilled.Text = "0";
+            // 
+            // LB_InnocentsKilled
+            // 
+            this.LB_InnocentsKilled.AutoSize = true;
+            this.LB_InnocentsKilled.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_InnocentsKilled.Location = new System.Drawing.Point(46, 219);
+            this.LB_InnocentsKilled.Name = "LB_InnocentsKilled";
+            this.LB_InnocentsKilled.Size = new System.Drawing.Size(121, 20);
+            this.LB_InnocentsKilled.TabIndex = 16;
+            this.LB_InnocentsKilled.Text = "Innocents Killed";
+            // 
+            // LB_Time
+            // 
+            this.LB_Time.AutoSize = true;
+            this.LB_Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Time.ForeColor = System.Drawing.Color.DimGray;
+            this.LB_Time.Location = new System.Drawing.Point(61, 67);
+            this.LB_Time.Name = "LB_Time";
+            this.LB_Time.Size = new System.Drawing.Size(90, 22);
+            this.LB_Time.TabIndex = 21;
+            this.LB_Time.Text = "00:00,000";
+            // 
+            // LB_MapName
+            // 
+            this.LB_MapName.AutoSize = true;
+            this.LB_MapName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_MapName.Location = new System.Drawing.Point(85, 47);
+            this.LB_MapName.Name = "LB_MapName";
+            this.LB_MapName.Size = new System.Drawing.Size(150, 20);
+            this.LB_MapName.TabIndex = 22;
+            this.LB_MapName.Text = "No mission currently";
+            // 
+            // NB_Headshots
+            // 
+            this.NB_Headshots.AutoSize = true;
+            this.NB_Headshots.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_Headshots.ForeColor = System.Drawing.Color.Blue;
+            this.NB_Headshots.Location = new System.Drawing.Point(12, 139);
+            this.NB_Headshots.Name = "NB_Headshots";
+            this.NB_Headshots.Size = new System.Drawing.Size(19, 20);
+            this.NB_Headshots.TabIndex = 24;
+            this.NB_Headshots.Tag = "Value";
+            this.NB_Headshots.Text = "0";
+            // 
+            // LB_Headshots
+            // 
+            this.LB_Headshots.AutoSize = true;
+            this.LB_Headshots.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Headshots.Location = new System.Drawing.Point(46, 139);
+            this.LB_Headshots.Name = "LB_Headshots";
+            this.LB_Headshots.Size = new System.Drawing.Size(87, 20);
+            this.LB_Headshots.TabIndex = 23;
+            this.LB_Headshots.Text = "Headshots";
+            // 
+            // NB_EnemiesHarmed
+            // 
+            this.NB_EnemiesHarmed.AutoSize = true;
+            this.NB_EnemiesHarmed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_EnemiesHarmed.ForeColor = System.Drawing.Color.Blue;
+            this.NB_EnemiesHarmed.Location = new System.Drawing.Point(12, 199);
+            this.NB_EnemiesHarmed.Name = "NB_EnemiesHarmed";
+            this.NB_EnemiesHarmed.Size = new System.Drawing.Size(19, 20);
+            this.NB_EnemiesHarmed.TabIndex = 26;
+            this.NB_EnemiesHarmed.Tag = "Value";
+            this.NB_EnemiesHarmed.Text = "0";
+            // 
+            // LB_EnemiesHarmed
+            // 
+            this.LB_EnemiesHarmed.AutoSize = true;
+            this.LB_EnemiesHarmed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_EnemiesHarmed.Location = new System.Drawing.Point(46, 199);
+            this.LB_EnemiesHarmed.Name = "LB_EnemiesHarmed";
+            this.LB_EnemiesHarmed.Size = new System.Drawing.Size(132, 20);
+            this.LB_EnemiesHarmed.TabIndex = 25;
+            this.LB_EnemiesHarmed.Text = "Enemies Harmed";
+            // 
+            // NB_InnocentsHarmed
+            // 
+            this.NB_InnocentsHarmed.AutoSize = true;
+            this.NB_InnocentsHarmed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NB_InnocentsHarmed.ForeColor = System.Drawing.Color.Blue;
+            this.NB_InnocentsHarmed.Location = new System.Drawing.Point(12, 239);
+            this.NB_InnocentsHarmed.Name = "NB_InnocentsHarmed";
+            this.NB_InnocentsHarmed.Size = new System.Drawing.Size(19, 20);
+            this.NB_InnocentsHarmed.TabIndex = 28;
+            this.NB_InnocentsHarmed.Tag = "Value";
+            this.NB_InnocentsHarmed.Text = "0";
+            // 
+            // LB_InnocentsHarmed
+            // 
+            this.LB_InnocentsHarmed.AutoSize = true;
+            this.LB_InnocentsHarmed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_InnocentsHarmed.Location = new System.Drawing.Point(46, 239);
+            this.LB_InnocentsHarmed.Name = "LB_InnocentsHarmed";
+            this.LB_InnocentsHarmed.Size = new System.Drawing.Size(141, 20);
+            this.LB_InnocentsHarmed.TabIndex = 27;
+            this.LB_InnocentsHarmed.Text = "Innocents Harmed";
+            // 
+            // LB_Timer
+            // 
+            this.LB_Timer.AutoSize = true;
+            this.LB_Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Timer.Location = new System.Drawing.Point(12, 68);
+            this.LB_Timer.Name = "LB_Timer";
+            this.LB_Timer.Size = new System.Drawing.Size(52, 20);
+            this.LB_Timer.TabIndex = 30;
+            this.LB_Timer.Text = "Time:";
+            // 
+            // LB_Mission
+            // 
+            this.LB_Mission.AutoSize = true;
+            this.LB_Mission.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Mission.Location = new System.Drawing.Point(12, 47);
+            this.LB_Mission.Name = "LB_Mission";
+            this.LB_Mission.Size = new System.Drawing.Size(74, 20);
+            this.LB_Mission.TabIndex = 31;
+            this.LB_Mission.Text = "Mission:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 272);
+            this.Controls.Add(this.LB_Mission);
+            this.Controls.Add(this.LB_Timer);
+            this.Controls.Add(this.NB_InnocentsHarmed);
+            this.Controls.Add(this.LB_InnocentsHarmed);
+            this.Controls.Add(this.NB_EnemiesHarmed);
+            this.Controls.Add(this.LB_EnemiesHarmed);
+            this.Controls.Add(this.NB_Headshots);
+            this.Controls.Add(this.LB_Headshots);
+            this.Controls.Add(this.LB_MapName);
+            this.Controls.Add(this.LB_Time);
+            this.Controls.Add(this.NB_InnocentsKilled);
+            this.Controls.Add(this.LB_InnocentsKilled);
+            this.Controls.Add(this.NB_EnemiesKilled);
+            this.Controls.Add(this.LB_EnemiesKilled);
+            this.Controls.Add(this.NB_Alerts);
+            this.Controls.Add(this.LB_Alerts);
+            this.Controls.Add(this.NB_CloseEncounters);
+            this.Controls.Add(this.LB_CloseEncounters);
+            this.Controls.Add(this.NB_ShotsFired);
+            this.Controls.Add(this.LB_Running);
+            this.Controls.Add(this.LB_ShotsFired);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hitman 2 Statistics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LB_ShotsFired;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label LB_Running;
+        private System.Windows.Forms.Label NB_ShotsFired;
+        private System.Windows.Forms.Label NB_CloseEncounters;
+        private System.Windows.Forms.Label LB_CloseEncounters;
+        private System.Windows.Forms.Label NB_Alerts;
+        private System.Windows.Forms.Label LB_Alerts;
+        private System.Windows.Forms.Label NB_EnemiesKilled;
+        private System.Windows.Forms.Label LB_EnemiesKilled;
+        private System.Windows.Forms.Label NB_InnocentsKilled;
+        private System.Windows.Forms.Label LB_InnocentsKilled;
+        private System.Windows.Forms.Label LB_Time;
+        private System.Windows.Forms.Label LB_MapName;
+        private System.Windows.Forms.Label NB_Headshots;
+        private System.Windows.Forms.Label LB_Headshots;
+        private System.Windows.Forms.Label NB_EnemiesHarmed;
+        private System.Windows.Forms.Label LB_EnemiesHarmed;
+        private System.Windows.Forms.Label NB_InnocentsHarmed;
+        private System.Windows.Forms.Label LB_InnocentsHarmed;
+        private System.Windows.Forms.Label LB_Timer;
+        private System.Windows.Forms.Label LB_Mission;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Hitman2Statistics
+{
+    public partial class Form1 : Form
+    {
+        // Base address value for pointers.
+        const int baseAddress = 0x00400000;
+
+        // Most values are accessed with 3-levels pointers and the second offset is different depending on the current mission.
+        // All second offsets are stored here to be accessed according to the correct mission.
+        int[] secondOffset = { 0x838, 0xB24, 0x8A0, 0x138, 0xB88, 0xBB8, 0xB48, 0xCE8, 0x136C, 0xAD0, 0xF50, 0x8D4, 0x9EC, 0x400, 0x9EC, 0x644, 0xB08, 0x96C, 0xB00, 0x8 };
+
+        // Dictionary used to convert the raw map names into easily readable names and a map number to access the second offsets declared previously.
+        Dictionary<string, Tuple<string, int>> mapValues = new Dictionary<string, Tuple<string, int>>() {
+            { "C1-1__MA", new Tuple<string, int>("Anathema", 1) },              { "C2-1__MA", new Tuple<string, int>("St. Petersburg Stakeout", 2) },   { "C2-2__MA", new Tuple<string, int>("Kirov Park Meeting", 3) },    { "C2-3__MA", new Tuple<string, int>("Tubeway Torpedo", 4) },               { "C2-4__MA", new Tuple<string, int>("Invitation to a Party", 5) },
+            { "C3-1__MA", new Tuple<string, int>("Tracking Hayamoto", 6) },     { "\\C3-2a__", new Tuple<string, int>("Hidden Valley", 7) },            { "\\C3-2b__", new Tuple<string, int>("At the Gates", 8) },         { "C3-3__MA", new Tuple<string, int>("Shogun Showdown", 9) },               { "C4-1__MA", new Tuple<string, int>("Basement Killing", 10) },
+            { "C4-2__MA", new Tuple<string, int>("The Graveyard Shift", 11) },  { "C4-3__MA", new Tuple<string, int>("The Jacuzzi Job", 12) },          { "C5-1__MA", new Tuple<string, int>("Murder At The Bazaar", 13) }, { "C5-2__MA", new Tuple<string, int>("The Motorcade Interception", 14) },   { "C5-3__MA", new Tuple<string, int>("Tunnel Rat", 15) },
+            { "C6-1__MA", new Tuple<string, int>("Temple City Ambush", 16) },   { "C6-2__MA", new Tuple<string, int>("The Death of Hannelore", 17) },   { "C6-3__MA", new Tuple<string, int>("Terminal Hospitality", 18) }, { "C7-1__MA", new Tuple<string, int>("St. Petersburg Revisited", 19) },     { "C8-1__MA", new Tuple<string, int>("Redemption at Gontranno", 20) }};
+
+        // Other variables.
+        System.Text.Encoding enc = System.Text.Encoding.UTF8;
+        Process[] myProcess;
+        String mapName;
+        int mapNumber;
+        float missionTime;
+        bool isMissionActive;
+        
+
+        /*------------------
+        -- INITIALIZATION --
+        ------------------*/
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /*------------------
+        -- MEMORY READING --
+        ------------------*/
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Attempt to find if the game is currently running.
+            myProcess = Process.GetProcessesByName("hitman2");
+
+            if (myProcess.Length != 0)
+            {
+                // The game is running, ready for memory reading.
+                LB_Running.Text = "HITMAN 2 IS RUNNING";
+                LB_Running.ForeColor = Color.Green;
+
+                // Reading the raw name of the current mission as an array of bytes and converting it to a string.
+                byte[] mapBytes = BitConverter.GetBytes(Trainer.ReadPointerDouble("hitman2", baseAddress + 0x002A6C5C, new int[2] { 0x98, 0xBC7 }));
+                string mapBytesStr = enc.GetString(mapBytes);
+                isMissionActive = true;
+
+                try
+                {
+                    // Trying to get the clean mission name and the mission number from the dictionary.
+                    mapName = mapValues[mapBytesStr].Item1;
+                    mapNumber = mapValues[mapBytesStr].Item2;
+                }
+                catch (KeyNotFoundException)
+                {
+                    // The mission name isn't included in the dictionary, meaning that a mission is not active at this moment.
+                    // The current screen is something like the main menu, the briefing or a cutscene.
+                    isMissionActive = false;
+                }
+
+                if (isMissionActive) // A mission is currently active, ready to read memory.
+                {
+                    // Reading the mission name with it's number.
+                    LB_MapName.Text = "#" + mapNumber + " " + mapName;
+
+                    // Reading the timer and displaying it with 3 decimals.
+                    missionTime = Trainer.ReadPointerFloat("hitman2", baseAddress + 0x2A6C5C, new int[1] { 0x24 });
+                    LB_Time.Text = ((int)missionTime / 60).ToString("D2") + ":" + (missionTime % 60).ToString("00.000");
+
+                    // Reading every other value, for some reason the number of shots fired isn't stored at the same place as all the other ones and is currently a bit glitchy.
+                    NB_ShotsFired.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x000421BC, new int[4] { 0x190, 0x714, 0x104, 0x667 }).ToString();
+                    NB_CloseEncounters.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x220 }).ToString();
+                    NB_Headshots.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x208 }).ToString();
+                    NB_Alerts.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x21C }).ToString();
+                    NB_EnemiesKilled.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x210 }).ToString();
+                    NB_EnemiesHarmed.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x20C }).ToString();
+                    NB_InnocentsKilled.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x218 }).ToString();
+                    NB_InnocentsHarmed.Text = Trainer.ReadPointerInteger("hitman2", baseAddress + 0x002A6C50, new int[3] { 0x28, secondOffset[mapNumber - 1], 0x214 }).ToString();
+                }
+                else // No mission is active, reseting values.
+                {
+                    ResetValues();
+                }
+            }
+            else
+            {
+                // The game process has not been found, reseting values.
+                LB_Running.Text = "HITMAN 2 IS NOT RUNNING";
+                LB_Running.ForeColor = Color.Red;
+                ResetValues();
+            }
+        }
+
+        // Called when the game is not running or no mission is active.
+        // Used to reset all the values.
+        private void ResetValues()
+        {
+            LB_Time.Text = "00:00,000";
+            LB_MapName.Text = "No mission currently";
+
+            NB_ShotsFired.Text = "0";
+            NB_CloseEncounters.Text = "0";
+            NB_Headshots.Text = "0";
+            NB_Alerts.Text = "0";
+            NB_EnemiesKilled.Text = "0";
+            NB_EnemiesHarmed.Text = "0";
+            NB_InnocentsKilled.Text = "0";
+            NB_InnocentsHarmed.Text = "0";
+        }
+    }
+}
